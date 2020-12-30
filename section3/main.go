9a6ea0a6507ce1fbd2640efd9d7d68f95a3111c4
cawauchi6204 <cawauchi6204@gmail.com>
2020-12-30 17:36:58 +0900

    feat(section3/): sec3完了

--- section3/main.go
+++ section3/main.go
@@ -1,11 +1,41 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+func outer() {
+	var s4 string = "outer"
+	fmt.Println(s4)
+}
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(time.Now())
+	var i int = 100
+	fmt.Println(i)
+
+	var s string = "Hello Go"
+	fmt.Println(s)
+
+	var t, f bool = true, false
+	fmt.Println(t, f)
+
+	var (
+		i2 int    = 200
+		s2 string = "Golang"
+	)
+	fmt.Println(i2, s2)
+
+	var i3 int
+	var s3 string
+	fmt.Println(i3, s3)
+
+	i3 = 300
+	s3 = "Go"
+	fmt.Println(i3, s3)
+
+	i = 150
+	fmt.Println(i)
+
+	i4 := 400
+	fmt.Println(i4)
+
+	outer()
 }
