95fd0f62dd173d98d672825fdccbd9ea8a67a0a1
cawauchi6204 <cawauchi6204@gmail.com>
2020-12-31 08:35:31 +0900

    4-16完了

--- section4/main.go
+++ section4/main.go
@@ -3,9 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var fl64 float64 = 2.4
-	fmt.Println(fl64)
+	var arr1 [3]int
+	fmt.Println(arr1)
+	fmt.Printf("%T\n", arr1)
 
-	fl := 3.2
-	fmt.Println(fl64 + fl)
+	var arr2 [3]string = [3]string{"A", "B"}
+	fmt.Println(arr2)
+
+	arr3 := [3]int{1, 2, 3}
+	fmt.Println(arr3)
+
+	arr4 := [...]string{"C", "D"}
+	fmt.Println(arr4)
+	fmt.Printf("%T\n", arr4)
+
+	fmt.Println(arr1[0])
+
+	arr2[2] = "C"
+	fmt.Println(arr2)
+
 }
