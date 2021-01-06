7699106276450b770154da341ccbd8baa620605c
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-06 20:03:56 +0900

    3-40完了

--- section8/main.go
+++ section8/main.go
@@ -1,37 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
-func TestDefer() {
-	defer fmt.Println("END")
-	fmt.Println("START")
-	// deferを使うことでTestDeferが終わった後にdefer分の処理を実行する
-}
-
-func RunDefer() {
-	defer fmt.Println("1")
-	defer fmt.Println("2")
-	defer fmt.Println("3")
-}
-
-// defer文が2件以上登録されている場合、したから実行される
-
+// panic recover
 func main() {
-	TestDefer()
-	// defer func() {
-	// 	fmt.Println("1")
-	// 	fmt.Println("2")
-	// 	fmt.Println("3")
-	// }()
-	RunDefer()
-
-	file, err := os.Create("test.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-	file.Write([]byte("Hello"))
+	defer func() {
+		if x := recover(); x != nil {
+			fmt.Println(x)
+		}
+	}()
+	panic("routime error")
+	fmt.Println("Start")
 }
