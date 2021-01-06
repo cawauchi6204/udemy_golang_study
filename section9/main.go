ac6036b751803c95f326e5579f381b09b4e1dcc5
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-06 21:19:45 +0900

    9-46完了

--- section9/main.go
+++ section9/main.go
@@ -5,19 +5,21 @@ import "fmt"
 // slice
 
 func main() {
-	sl := []int{100, 200}
-	fmt.Println(sl)
+	// sl := []int{100, 200}
+	// sl2 := sl
 
-	sl = append(sl, 300)
-	fmt.Println(sl)
+	// sl2[0] = 1000
+	// fmt.Println(sl)
 
-	sl = append(sl, 400, 500, 600)
-	fmt.Println(sl)
+	// var i int = 10
+	// i2 := i
+	// i2 = 100
+	// fmt.Println(i, i2)
 
-	sl2 := make([]int, 5)
+	sl := []int{1, 2, 3, 4, 5}
+	sl2 := make([]int, 5, 10)
 	fmt.Println(sl2)
+	n := copy(sl2, sl)
 
-	fmt.Println(len(sl2))
-
-	fmt.Println(cap(sl2))
+	fmt.Println(n, sl2)
 }
