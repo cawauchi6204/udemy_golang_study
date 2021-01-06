e15a3f84bc5eea0543795b906bc96fc963cb51a8
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-06 20:23:54 +0900

    section9完了

---
+++ section9/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// slice
+
+func main() {
+	var sl []int
+	fmt.Println(sl)
+
+	var sl2 []int = []int{100, 200}
+	fmt.Println(sl2)
+
+	sl3 := []string{"A", "B"}
+	fmt.Println(sl3)
+
+	sl4 := make([]int, 5)
+	fmt.Println(sl4)
+
+	sl2[0] = 1000
+	fmt.Println(sl2)
+
+	sl5 := []int{1, 2, 3, 4, 5}
+	fmt.Println(sl5)
+
+	fmt.Println(sl5[0])
+
+	fmt.Println(sl5[:2])
+
+	fmt.Println(sl5[:])
+
+	fmt.Println(len(sl5) - 1)
+}
