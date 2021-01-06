ca3c8c576a1ec409f181aa8ee986bac1dcdfeb22
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-06 19:59:32 +0900

    8-39完了

--- section8/main.go
+++ section8/main.go
@@ -1,29 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func TestDefer() {
+	defer fmt.Println("END")
+	fmt.Println("START")
+	// deferを使うことでTestDeferが終わった後にdefer分の処理を実行する
+}
+
+func RunDefer() {
+	defer fmt.Println("1")
+	defer fmt.Println("2")
+	defer fmt.Println("3")
+}
+
+// defer文が2件以上登録されている場合、したから実行される
 
 func main() {
-	// Loop:
-	// 	for {
-	// 		for {
-	// 			for {
-	// 				fmt.Println("START")
-	// 				break Loop
-	// 			}
-	// 			fmt.Println("処理をしない")
-	// 		}
-	// 		fmt.Println("処理をしない")
-	// 	}
-	// 	fmt.Println("END")
+	TestDefer()
+	// defer func() {
+	// 	fmt.Println("1")
+	// 	fmt.Println("2")
+	// 	fmt.Println("3")
+	// }()
+	RunDefer()
 
-Loop:
-	for i := 0; i < 3; i++ {
-		for j := 1; j < 3; j++ {
-			if j > 1 {
-				continue Loop
-			}
-			fmt.Println(i, j, i*j)
-		}
-		fmt.Println("処理をしない")
+	file, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Println(err)
 	}
+	defer file.Close()
+	file.Write([]byte("Hello"))
 }
