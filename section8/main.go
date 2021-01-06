e7dea10947a71791a01c3faf8a84cfe3cda3d314
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-06 20:08:20 +0900

    8-41完了

--- section8/main.go
+++ section8/main.go
@@ -1,14 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-// panic recover
+// go goroutin
+
+func sub() {
+	for {
+		fmt.Println("Sub Loop")
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
 func main() {
-	defer func() {
-		if x := recover(); x != nil {
-			fmt.Println(x)
-		}
-	}()
-	panic("routime error")
-	fmt.Println("Start")
+	go sub()
+	go sub()
+
+	for {
+		fmt.Println("Main Loop")
+		time.Sleep(200 * time.Millisecond)
+	}
 }
