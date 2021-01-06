8908687fc83f9f7830fe9ad65f6a4a82e9881ec0
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-06 21:14:51 +0900

    9-45

--- section9/main.go
+++ section9/main.go
@@ -5,29 +5,19 @@ import "fmt"
 // slice
 
 func main() {
-	var sl []int
+	sl := []int{100, 200}
 	fmt.Println(sl)
 
-	var sl2 []int = []int{100, 200}
+	sl = append(sl, 300)
+	fmt.Println(sl)
+
+	sl = append(sl, 400, 500, 600)
+	fmt.Println(sl)
+
+	sl2 := make([]int, 5)
 	fmt.Println(sl2)
 
-	sl3 := []string{"A", "B"}
-	fmt.Println(sl3)
+	fmt.Println(len(sl2))
 
-	sl4 := make([]int, 5)
-	fmt.Println(sl4)
-
-	sl2[0] = 1000
-	fmt.Println(sl2)
-
-	sl5 := []int{1, 2, 3, 4, 5}
-	fmt.Println(sl5)
-
-	fmt.Println(sl5[0])
-
-	fmt.Println(sl5[:2])
-
-	fmt.Println(sl5[:])
-
-	fmt.Println(len(sl5) - 1)
+	fmt.Println(cap(sl2))
 }
