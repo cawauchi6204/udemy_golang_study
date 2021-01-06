2912d6e0ebe3bfef70a57a570ce89f82edef188e
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-06 19:51:57 +0900

    3-38完了

--- section8/main.go
+++ section8/main.go
@@ -3,30 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	// n := 5
-	// switch n {
-	// case 1, 2:
-	// 	fmt.Println("1 or 2")
-	// case 3, 4:
-	// 	fmt.Println("3 or 4")
-	// default:
-	// 	fmt.Println("I dont know")
-	// }
+	// Loop:
+	// 	for {
+	// 		for {
+	// 			for {
+	// 				fmt.Println("START")
+	// 				break Loop
+	// 			}
+	// 			fmt.Println("処理をしない")
+	// 		}
+	// 		fmt.Println("処理をしない")
+	// 	}
+	// 	fmt.Println("END")
 
-	// switch n := 2; n {
-	// case 1, 2:
-	// 	fmt.Println("1 or 2")
-	// case 3, 4:
-	// 	fmt.Println("3 or 4")
-	// default:
-	// 	fmt.Println("I dont know")
-	// }
-	n := 6
-	switch {
-	case n > 0 && n < 4:
-		fmt.Println("0 < n < 4")
-	case n > 3 && n < 7:
-		fmt.Println("3 < n < 7")
+Loop:
+	for i := 0; i < 3; i++ {
+		for j := 1; j < 3; j++ {
+			if j > 1 {
+				continue Loop
+			}
+			fmt.Println(i, j, i*j)
+		}
+		fmt.Println("処理をしない")
 	}
-
 }
