f26a8261e8d8b7ab7300c31b293e04a009208c4d
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-08 00:04:14 +0900

    9-49

--- section9/main.go
+++ section9/main.go
@@ -2,18 +2,41 @@ package main
 
 import "fmt"
 
-// slice
+// map
 
 func main() {
-	sl := []string{"A", "B", "C"}
-	fmt.Println(sl)
+	var m = map[string]int{"A": 100, "B": 200}
 
-	// for i := range sl {
-	// 	fmt.Println(i)
-	// }
+	fmt.Println(m)
 
-	for i := 0; i < len(sl); i++ {
-		fmt.Println(sl[i])
+	m2 := map[string]int{"A": 100, "B": 200}
+
+	fmt.Println(m2)
+
+	m3 := map[int]string{
+		1: "A",
+		2: "B",
+	}
+	fmt.Println(m3)
+
+	m4 := make(map[int]string)
+	fmt.Println(m4)
+
+	m4[1] = "JAPAN"
+	m4[2] = "USA"
+	fmt.Println(m4)
+
+	fmt.Println(m["A"])
+	fmt.Println(m4[2])
+
+	_, ok := m4[1]
+	if !ok {
+		fmt.Println("error")
+	} else {
+		fmt.Println("ok")
 	}
 
+	delete(m4, 2)
+	fmt.Println(m4)
+	// fmt.Println(s, ok)
 }
