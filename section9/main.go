24d65c667ac2e35a4384981e52c279858d874c81
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-08 00:06:13 +0900

    9-50完了

--- section9/main.go
+++ section9/main.go
@@ -5,38 +5,12 @@ import "fmt"
 // map
 
 func main() {
-	var m = map[string]int{"A": 100, "B": 200}
-
-	fmt.Println(m)
-
-	m2 := map[string]int{"A": 100, "B": 200}
-
-	fmt.Println(m2)
-
-	m3 := map[int]string{
-		1: "A",
-		2: "B",
-	}
-	fmt.Println(m3)
-
-	m4 := make(map[int]string)
-	fmt.Println(m4)
-
-	m4[1] = "JAPAN"
-	m4[2] = "USA"
-	fmt.Println(m4)
-
-	fmt.Println(m["A"])
-	fmt.Println(m4[2])
-
-	_, ok := m4[1]
-	if !ok {
-		fmt.Println("error")
-	} else {
-		fmt.Println("ok")
+	m := map[string]int{
+		"Apple":  100,
+		"Banana": 200,
 	}
 
-	delete(m4, 2)
-	fmt.Println(m4)
-	// fmt.Println(s, ok)
+	for k := range m {
+		fmt.Println(k)
+	}
 }
